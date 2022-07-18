1f86d8992a4ba488fdcf499198de8547d3bc7a2a
Software Engineer <97345885+Gangstar0312@users.noreply.github.com>
2022-07-18 11:20:44 +0200

    modify

--- src/App.vue
+++ src/App.vue
@@ -7,42 +7,81 @@
 
   <br>
 
+  <button @click="ProcessQueue()">Process Queue</button>
+  <br>
+
   <button @click="clearPendingJobs()">Clear Pending Jobs</button>
 
   <br>
 
   <button @click="clearCompletedJobs()">Clear Completed Jobs</button>
 
-  <queue-controller/>
+  
+    <div>
+        <h2>Active</h2>
+        <div>{{ active.id }}</div>
+
+        <div style="display: flex; margin-top: 20px; align-items: start; justify-content: space-between; width: 75%; margin: auto">
+            <div>
+                <h2>Pending</h2>
+                <div v-for="( job, index ) in pending"
+                    v-bind:key="'pending-'+index">
+                        {{ job }}
+
+                        <button v-on:click="cancelJob( index )">Cancel Job</button>
+                </div>
+            </div>
+            <div>
+                <h2>Completed</h2>
+                <div v-for="( job, index ) in completed"
+                    v-bind:key="'completed-'+index">
+                        {{ job }}
+                </div>
+            </div>
+        </div>
+        
+    </div>
 
 </template>
 
 <script>
 
-import QueueController from './components/QueueController.vue';
-
+// import QueueController from './components/QueueController.vue';
+import { mapState } from 'vuex';
 export default {
   name: 'App',
-  components: {
-    QueueController
+  // components: {
+  //   QueueController
+  // },
+  data() {
+    return {
+      id: 0,
+    }
   },
+  computed: {
+        ...mapState('queue', {
+            pending: 'pending',
+            active: 'active',
+            completed: 'completed'
+        })
+    },
 
+  watch: {
+        active(){
+            this.processJob();
+        }
+    },
   methods: {
     populateQueue(){
-      for( let i = 0; i < 50; i++ ){
+        this.id++
         this.$store.dispatch('queue/addJob', {
-          id: i,
-          handler: function( ){
-            // If you run this, just to slow down so the jobs just don't
-            // complete instantly.
-            setTimeout(function(){
-              // Your functionality here.
-              this.$store.dispatch('queue/startNextJob');
-            }.bind(this), 3000);
-          }.bind(this)
+          id: this.id
         });
-      }
     },
+
+    ProcessQueue() {
+      this.$store.dispatch('queue/startNextJob');
+    },  
 
     clearPendingJobs(){
       this.$store.dispatch('queue/clearPendingJobs');
@@ -50,7 +89,17 @@ export default {
 
     clearCompletedJobs(){
       this.$store.dispatch('queue/clearCompletedJobs');
-    }
+    },
+
+    processJob(){
+            if( this.active.handler ){
+                this.active.handler();
+            }
+        },
+
+        cancelJob( index ){
+            this.$store.dispatch('queue/cancelJob', index );
+        }
   }
 }
 </script>
